8ef9d667e06f2562c5ffc77e6d66ac015e29ee8d
gorishaikh <Nasreenpeera.shaikh@gmail.com>
2024-06-21 07:11:44 +0530

    update code

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ EOF
 }
 
 resource "aws_instance" "web" {
-  ami             = ""ami-0eaf7c3456e7b5b68
+  ami             = "ami-0eaf7c3456e7b5b68"
   instance_type   = "t2.medium" 
   key_name        = var.key_name
   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
